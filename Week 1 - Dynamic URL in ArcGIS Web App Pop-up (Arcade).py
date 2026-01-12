82148621c2274ce2e76a0bf318e6ec4e28f90a73
Nathan R <Nr1864@gmail.com>
2026-01-11 20:12:50 -0500

    Add files via upload

---
+++ Week 1 - Dynamic URL in ArcGIS Web App Pop-up (Arcade).py
@@ -0,0 +1,21 @@
+var url = "https://assessor.berkeleycountysc.gov/property_card.php?tms=" + $feature.Parcel_Num
+var label1 = "Property Card:"
+var label2 = "Click to View Property Card"
+
+{
+  "type": "text",
+  "text": label1 + "<br><a href='" + url + "' target='_blank'>" + label2 + "</a>"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
