30ba8a55fa6c7c700839fa94be91fc2d4d1579d0
Nathan R <Nr1864@gmail.com>
2026-01-11 20:19:07 -0500

    Update Week 1 - Dynamic URL in ArcGIS Web App Pop-up (Arcade).py

--- Week 1 - Dynamic URL in ArcGIS Web App Pop-up (Arcade).py
+++ Week 1 - Dynamic URL in ArcGIS Web App Pop-up (Arcade).py
@@ -1,3 +1,12 @@
+/*
+Creates a dynamic URL in ArcGIS web app pop-up.
+To use, add an arcade element into ArcGIS popup and paste code inside.
+
+Created by Nathan Ryan
+Please give attribution!
+*/
+
+// Variables. Edit me!
 var url = "https://assessor.berkeleycountysc.gov/property_card.php?tms=" + $feature.Parcel_Num
 var label1 = "Property Card:"
 var label2 = "Click to View Property Card"
@@ -6,16 +15,3 @@ var label2 = "Click to View Property Card"
   "type": "text",
   "text": label1 + "<br><a href='" + url + "' target='_blank'>" + label2 + "</a>"
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
